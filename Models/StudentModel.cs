7fcf597145a8d0996097cae56cc6536c5fc56a37
Klinaren <lehenkyjwitalik@gmail.com>
2026-04-28 08:57:26 +0200

    Student_Drawing_Machine_VL_4F

---
+++ Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Student_Drawing_System.Models
+{
+    public class StudentModel
+    {
+        public string? Name { get; set; }
+        public string? FamilyName { get; set; }
+        public string? ClassName { get; set; }
+    }
+}
